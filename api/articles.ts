0422f1c22bf94336d01aa830c0597e5c326ad1ab
Elena Fodorova <ena.fodorova@gmail.com>
2025-04-07 18:16:47 +0200

    Remove redundant code

--- api/articles.ts
+++ api/articles.ts
@@ -2,9 +2,7 @@ import { ArticleDetail, NewArticlePayload } from "@/types/Article"
 
 import client from "./client"
 
-export const postArticle = async (
-  body: NewArticlePayload & { imageId: string },
-) => {
+export const postArticle = async (body: NewArticlePayload) => {
   const response = await client.post("/articles", body)
 
   return response.data as ArticleDetail
