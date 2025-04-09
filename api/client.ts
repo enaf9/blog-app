fe5e4f1b0a90067ad72fc7942bea36e1dae397a9
Elena Fodorova <ena.fodorova@gmail.com>
2025-04-09 16:43:34 +0200

    Refactor client and authorization header

--- api/client.ts
+++ api/client.ts
@@ -5,9 +5,18 @@ const client = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api",
   headers: {
     "Content-Type": "application/json",
-    "X-API-KEY": process.env.NEXT_PUBLIC_API_KEY,
-    Authorization: `Bearer ${Cookies.get("token")}`
+    "X-API-KEY": process.env.NEXT_PUBLIC_API_KEY
   }
+})
+
+client.interceptors.request.use(request => {
+  const token = Cookies.get("token")
+
+  if (token) {
+    request.headers["Authorization"] = `Bearer ${token}`
+  }
+
+  return request
 })
 
 client.interceptors.response.use(
